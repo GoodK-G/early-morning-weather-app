7a277b70d997764061ac851fb9537480a615c837
GoodK-G <141077856+GoodK-G@users.noreply.github.com>
2024-02-08 19:04:56 +0000

    added the fetch for the two api's

--- js/home.js
+++ js/home.js
@@ -16,4 +16,42 @@ var formSubmitHandler = function (event) {
     }
   };
 
+  var get5Day = function (data){
+    var apiURL5day = 'api.openweathermap.org/data/2.5/forecast?lat='+data.lat + '&lon='+data.lon+'&appid='+ APIKey;
+    fetch(apiURL5day)
+    .then(function(response){
+        if (response.ok){
+            response.json().then(function(data){
+                console.log(data)
+            });
+           
+        } else{
+           alert('error' + response.statusText);   
+        }
+        })
+        .catch(function (error) {
+            alert('Unable to fetch');
+        })
+  };
+
+  var getWeather = function (city) {
+    var apiUrlGeo = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid='+APIKey;
+    
+    fetch(apiUrlGeo)
+      .then(function (response) {
+        if (response.ok) {
+          console.log(response);
+          response.json().then(function (data) {
+            get5Day(data);
+            console.log(data);
+            
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to fetch');
+      });
+  };
   
